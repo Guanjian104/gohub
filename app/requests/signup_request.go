a56fb10e369bb04245f3c62a7205157a58efaea9
guanjian <guanjian104@163.com>
2022-07-22 16:27:45 +0800

    JSON 解析和验证器封装

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -9,12 +9,11 @@ type SignupPhoneExistRequest struct {
 	Phone string `json:"phone,omitempty" valid:"phone"`
 }
 
-func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
+func SignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
 	// 自定义验证规则
 	rules := govalidator.MapData{
 		"phone": []string{"required", "digits:11"},
 	}
-
 	// 自定义验证出错时的提示
 	messages := govalidator.MapData{
 		"phone": []string{
@@ -23,28 +22,18 @@ func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]str
 		},
 	}
 
-	// 配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-        Rules:         rules,
-        TagIdentifier: "valid", // 模型中的 Struct 标签标识符
-        Messages:      messages,
-	}
-
-	// 开始验证
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data, rules, messages)
 }
 
 type SignupEmailExistRequest struct {
 	Email string `json:"email,omitempty" valid:"email"`
 }
 
-func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
+func SignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
 	// 自定义验证规则
 	rules := govalidator.MapData{
 		"email": []string{"required", "min:4", "max:30", "email"},
 	}
-
 	// 自定义验证出错时的提示
 	messages := govalidator.MapData{
 		"email": []string{
@@ -55,14 +44,5 @@ func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]str
         },
 	}
 
-	// 配置初始化
-	opts := govalidator.Options{
-        Data:          data,
-        Rules:         rules,
-        TagIdentifier: "valid", // 模型中的 Struct 标签标识符
-        Messages:      messages,
-    }
-
-	// 开始验证
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data, rules, messages)
 }
