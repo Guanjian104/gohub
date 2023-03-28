d4e7c88db12854b15223ff0dde762123fbee8707
gj <guanjian104@163.com>
2023-03-28 18:03:59 +0800

    make cmd 命令

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"gohub/app/cmd"
+    "gohub/app/cmd/make"
 	"gohub/bootstrap"
 	btsConfig "gohub/config"
 	"gohub/pkg/config"
@@ -43,6 +44,7 @@ func main() {
         cmd.CmdServe,
         cmd.CmdKey,
         cmd.CmdPlay,
+        make.CmdMake,
     )
 
     // 配置默认运行 Web 服务
